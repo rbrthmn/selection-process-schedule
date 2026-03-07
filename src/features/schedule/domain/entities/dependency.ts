c22916e3cf1f6f2d3c21c556fe6bf2f1b0adc954
rbrthmn <robertokenzohamano@gmail.com>
2026-03-07 12:08:18 -0300

    refactor entities

--- src/features/schedule/domain/entities/dependency.ts
+++ src/features/schedule/domain/entities/dependency.ts
@@ -1,26 +1,9 @@
-import {AppError} from "../../../../core/errors/custom-error";
-
 export class Dependency {
     constructor(
-        public source: string,
-        public target: string
+        public id: string,
+        public event: Event,
+        public previousEvent: Event,
+        public dislocationDays: number
     ) {
-    }
-
-    public static fromJson(obj: Record<string, unknown>): Dependency {
-        const {source, target} = obj;
-        if (!source) {
-            throw AppError.badRequest('This entity requires a source', [{
-                constraint: 'source is required',
-                fields: ['source']
-            }]);
-        }
-        if (!target) {
-            throw AppError.badRequest('This entity requires a target', [{
-                constraint: 'target is required',
-                fields: ['target']
-            }]);
-        }
-        return new Dependency(source as string, target as string);
     }
 }
--- src/features/schedule/domain/entities/event.ts
+++ src/features/schedule/domain/entities/event.ts
@@ -1,26 +1,12 @@
-import {AppError} from "../../../../core/errors/custom-error";
-
 export class Event {
     constructor(
+        public id: string,
         public name: string,
-        public duration: number
+        public type: string,
+        public initialDate: Date,
+        public endDate: Date,
+        public dependencies: Event[],
+        public durationDays: number,
     ) {
-    }
-
-    public static fromJson(obj: Record<string, unknown>): Event {
-        const {name, duration} = obj;
-        if (!name) {
-            throw AppError.badRequest('This entity requires a name', [{
-                constraint: 'name is required',
-                fields: ['name']
-            }]);
-        }
-        if (duration === undefined || duration === null) {
-            throw AppError.badRequest('This entity requires a duration', [{
-                constraint: 'duration is required',
-                fields: ['duration']
-            }]);
-        }
-        return new Event(name as string, duration as number);
     }
 }
